f0362f0d6d88e9824716ec62f6a360489b3a6235
Ewan <etavener@hotmail.com>
2020-06-10 18:46:25 +0100

    resolve dynamic pages for scully

--- scully.learnario.config.js
+++ scully.learnario.config.js
@@ -4,5 +4,13 @@ exports.config = {
     projectRoot: "./src",
     projectName: "learnario",
     outDir: './dist/static',
-    routes: {}
+    routes: {
+        '/:post-slug': {
+            type: 'json',
+            'post-slug': {
+                url: 'https://wp.learnario.com/wp-json/wp/v2/posts?per_page=100',
+                property: 'slug'
+            }
+        }
+    }
 };
--- scully.learnario.config.ts
+++ scully.learnario.config.ts
@@ -4,5 +4,12 @@ export const config: ScullyConfig = {
   projectName: "learnario",
   outDir: './dist/static',
   routes: {
+    '/:post-slug': {
+      type: 'json',
+      'post-slug': {
+        url: 'https://wp.learnario.com/wp-json/wp/v2/posts?per_page=100',
+        property: 'slug'
+      }
+    }
   }
 };
