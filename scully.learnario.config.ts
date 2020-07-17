f525249b5c71f17b25d4e08178c4243a59fba6d4
Ewan <etavener@hotmail.com>
2020-07-17 13:31:16 +0100

    updated dependency

--- scully.learnario.config.ts
+++ scully.learnario.config.ts
@@ -5,11 +5,7 @@ import { MinifyHtml } from 'scully-plugin-minify-html';
 
 const SitemapPlugin = getSitemapPlugin();
 setPluginConfig(SitemapPlugin, {
-  urlPrefix: 'https://learnario.com',
-  sitemapFilename: 'sitemap.xml',
-  changeFreq: 'monthly',
-  priority: ['1.0', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3', '0.2', '0.1', '0.0'],
-  ignoredRoutes: ['/404']
+  urlPrefix: 'https://demo.learnario.com'
 });
 
 export const config: ScullyConfig = {
